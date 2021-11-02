9c7678169a75fa3205f748eeca8670a534f458fb
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-11-02 10:20:20 -0400

    Work on HW 5-3

--- hw5-3.py
+++ hw5-3.py
@@ -1,7 +1,13 @@
 # Author: CRS 10/29/21
+import calendar
 # Question 1
-'''python
-    calendar.TextCalendar().pryear(2020)
-    '''
+calendar.TextCalendar().pryear(2020)
 
 # Question 2
+calendar.TextCalendar(6).pryear(2020)
+
+# Question 3
+calendar.TextCalendar().prmonth(2021, 11)
+
+# Question 5
+print(calendar.isleap)
